b6c6ea36b5fc1f87143837e08943dfc8cb9250a2
RomanAisly <romanalexaisly@gmail.com>
2024-11-11 18:51:06 +0200

    first commit

---
+++ app/src/main/java/com/moves/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.moves
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.moves.ui.theme.MovesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MovesTheme {
+
+            }
+        }
+    }
+}
